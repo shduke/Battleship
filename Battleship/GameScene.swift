c2959d459b8539af9a9f61e0607b336b5353c23a
herbertli <herbertli@users.noreply.github.com>
2016-01-01 17:31:51 -0500

    timer
    added timer

--- Battleship/GameScene.swift
+++ Battleship/GameScene.swift
@@ -11,12 +11,30 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
+        myLabel.text = "Hello World!";
         myLabel.fontSize = 45;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        self.addChild(myLabel);
         
-        self.addChild(myLabel)
+        /* timer */
+        // setup
+        let scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.fontSize = 45;
+        scoreLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        // wait one second between each increment
+        let actionwait = SKAction.waitForDuration(1.0)
+        var timesecond = Int()
+        // increment timer
+        let actionrun = SKAction.runBlock({
+            timesecond++
+            scoreLabel.text = String(timesecond);
+        })
+        // repeats sequence forever
+        scoreLabel.runAction(SKAction.repeatActionForever(SKAction.sequence([actionwait,actionrun])))
+        // add node to scene
+        self.addChild(scoreLabel);
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
